80f2e1ff01b4624f5ea93e9818f9d1fc6bf7c003
Jarrod Parkes <jarrod@udacity.com>
2015-10-19 08:43:32 -0500

    Fix initial code for AA 3

--- Alien Adventure/AddBadge.swift
+++ Alien Adventure/AddBadge.swift
@@ -9,6 +9,11 @@
 extension UDGameSM {
     
     func addBadge(hero: Hero, alien: Alien) {
-
+        switch(alien.colorVariant) {
+        case .Magenta:
+            hero.addBadge(Badge(requestType: alien.currentRequestType))
+        case .Teal:
+            hero.addBadge(SpecialBadge(requestType: alien.currentRequestType))
+        }
     }
 }
